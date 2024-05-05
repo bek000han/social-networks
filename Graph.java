8ec876d3145eaad0bcd01418d52cf727a07dbb41
bek000han <bekuniversity@gmail.com>
2024-05-05 17:35:43 +0100

    testing first half

--- Graph.java
+++ Graph.java
@@ -9,38 +9,42 @@ public class Graph {
         this.head = null;
     }
 
-    public void addFromFile(String fileToRead) throws FileNotFoundException {
+    public void addFromFile(String fileToRead) {
 
         // Gets all users line by line  from file and splits into String array
-        File file = new File(fileToRead);
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] users = line.split(line);
+        try {
+            File file = new File(fileToRead);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] users = line.split(line);
 
-            // first user in the file line is the head node
-            // created if does not exist
-            Node headNode = findNode(users[0]);
-            if (headNode == null) {
-                Node newNode = new Node(users[0]); 
-                addNode(newNode);
-            }
+                // first user in the file line is the head node
+                // created if does not exist
+                Node headNode = findNode(users[0]);
+                if (headNode == null) {
+                    Node newNode = new Node(users[0]); 
+                    addNode(newNode);
+                }
 
-            // attaches the head to the follows list of the users
-            // if they don't exist they get added as nodes
-            // skips operation if no users followed (array size 1)
-            if (users.length > 1) {
-                for (int i = 1; i < users.length; i++) {
-                    Node targetNode = findNode(users[i]);
-                    if (targetNode == null) {
-                        targetNode = new Node(users[i]); 
-                        addNode(targetNode);
+                // attaches the head to the follows list of the users
+                // if they don't exist they get added as nodes
+                // skips operation if no users followed (array size 1)
+                if (users.length > 1) {
+                    for (int i = 1; i < users.length; i++) {
+                        Node targetNode = findNode(users[i]);
+                        if (targetNode == null) {
+                            targetNode = new Node(users[i]); 
+                            addNode(targetNode);
+                        }
+                        targetNode.addFollow(headNode);
                     }
-                    targetNode.addFollow(headNode);
                 }
             }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
         }
-        scanner.close();
     }
 
     // sets head if graph is empty || pushes list down and sets new head
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
         Graph Dapper = new Graph();
+        Dapper.addFromFile(args[0]);
+        GraphTool tool = new GraphTool(Dapper);
+        System.out.println("Density: " + tool.density());
     }
 }
